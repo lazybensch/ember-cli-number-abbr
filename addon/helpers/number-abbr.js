fc869b41d5d6ba5c211adada41b29323f18530b0
lazybensch <lazybensch@gmail.com>
2015-08-01 10:22:39 +0200

    ignore jshint destructuring bug

--- addon/helpers/number-abbr.js
+++ addon/helpers/number-abbr.js
@@ -14,7 +14,7 @@ function getDecimalPlaces(adjustedNumber, maxSignificantPlaces) {
   return decimalPlaces > 0 ? decimalPlaces : 0;
 }
 
-export function numberAbbr([number = 0, delimiter = '.', maxSignificantPlaces = 3, padding = false, abbreviations = null], options) {
+export function numberAbbr([number = 0, delimiter = '.', maxSignificantPlaces = 3, padding = false, abbreviations = null], options) { // jshint ignore:line
 
   let symbols = abbreviations || Ember.A(['', 'K', 'M', 'B', 'T', 'Qua', 'Qui', 'Sex', 'Sep', 'Oct', 'Non', 'Dec']);
 
